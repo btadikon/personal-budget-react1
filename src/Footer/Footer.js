0f7deb2b19450d24b2b3b53c311f84edb77b0aae
Bharadwaaja Tadikonda <btadikon@uncc.edu>
2024-02-18 18:25:58 -0800

    completed react instructions

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+    return (
+        <footer className="bottom">
+            <div className="center">
+                Changes done by Bharadwaaja Tadikonda.
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
